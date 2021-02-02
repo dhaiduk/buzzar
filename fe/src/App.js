cc99e6f73e6c94f150442e469a3c537e568c5903
Dzmitry Haiduk <dementeyyy@gmail.com>
2021-02-02 14:44:41 +0300

    Viewed component

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,23 +1,43 @@
 //import logo from './logo.svg';
 import React from 'react';
-//import ReactDOM from 'react-dom'
+//import ReactDOM from 'react-dom';
+//import CircularProgress from '@material-ui/core/CircularProgress';
 import './App.css';
 import * as THREE from "three";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { message: "hello" };
+  }
+
+  changeState() {
+    this.setState({
+      message: "hi"
+    });
+  }
   componentDidMount() {
-    var scene  = document.querySelector('a-scene').object3D;
-    var geometry = new THREE.BoxGeometry( 0.1, 0.1, 0.1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    var cube = new THREE.Mesh( geometry, material );
+    var scene = document.querySelector('a-scene').object3D;
+    var geometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
+    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    var cube = new THREE.Mesh(geometry, material);
     cube.position.set(0, 0.2, 0.1);;
-    scene.add( cube );
+    scene.add(cube);
     console.log(scene)
-    document.querySelector('a-scene').addEventListener('loaded', function () {console.log("a-scene loaded")})
+
   };
 
+  componentDidUpdate()
+  { console.log("componentDidUpdate")
+  this.setState({
+    message: "hi"
+  });
+  }
+
   render() {
-    return <div ref={ref => (this.mount = ref)} />;
+    return <div className="App">
+      {this.state.message}
+    </div>;
   }
 }
 
