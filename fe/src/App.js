1cffca9a9b6502bc3d238e2fc4995a4b2ebe3cf4
Dzmitry Haiduk <dementeyyy@gmail.com>
2021-02-02 12:50:40 +0300

    GLB loaded

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,24 +1,24 @@
+//import logo from './logo.svg';
 import React from 'react';
+//import ReactDOM from 'react-dom'
 import './App.css';
-import 'aframe';
-import {Entity, Scene} from 'aframe-react';
-function App() {
-  return (
-    <div className="App">
-     <Scene className = {"a-frame-wrapper"}
-      light = "defaultLightEnable: false"
-      inspector = "url: https://aframe.io/releases/0.4.0/aframe-inspector.min.js"
-      >
-      <Entity id = "wall" primitive="a-box" color= {"red"}
-        position={{x:0, y:0, z:-25}}
-        scale={{x:20, y:50, z:2}}>
-      </Entity>
-      <Entity id = "skyElemet" primitive="a-sky"  radius = {"1000"} color={"#6EBAA7"}
-        scale={{x:1, y:1, z:1}}>
-      </Entity>
-     </Scene>
-    </div>
-  );
+import * as THREE from "three";
+
+class App extends React.Component {
+  componentDidMount() {
+    var scene  = document.querySelector('a-scene').object3D;
+    var geometry = new THREE.BoxGeometry( 0.1, 0.1, 0.1 );
+    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    var cube = new THREE.Mesh( geometry, material );
+    cube.position.set(0, 0.2, 0.1);;
+    scene.add( cube );
+    console.log(scene)
+    document.querySelector('a-scene').addEventListener('loaded', function () {console.log("a-scene loaded")})
+  };
+
+  render() {
+    return <div ref={ref => (this.mount = ref)} />;
+  }
 }
 
 export default App;
